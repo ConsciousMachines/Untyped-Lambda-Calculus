5517ad2cdd734b7fed7f49de83990b48f180bca5
ConsciousMachines <pwnagecorp2@gmail.com>
2021-05-09 06:01:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using name = System.String;
+
+
+namespace lambda_calc
+{
+    public static class T // tools
+    {
+        public static int ac = 0; // counter for names (counts how many alpha-conversions we do)
+        public static int br = 0; // counter for # of beta-reductions
+        public static int os = 0; // counter for # of beta-reductions skipped by optimization
+        public static void assert(bool b, string m = "") { if (!b) throw new Exception(m); }
+        public static string generate_name(name n) => $"{n}{ac++}";
+    }
+    public class Parser
+    {
+        private string fn_name;
+        private string fn_body;
+        private int i; // positions in the string 
+        // Numeric 48 -> 57 Capitals 65 -> 90 Lowers 97 -> 122 Underscore
+        public static bool is_alphanumeric(char c) => (!((c < 48) || (c > 57 && c < 65) || (c > 90 && c < 97) || (c > 122)) || (c == '_'));
+        // look at the current char
+        private char curr_char() => (i < fn_body.Length) ? fn_body[i] : '$';
+        // advance the stream by 1 char, and view it 
+        private char next_char() { i++; return curr_char(); }
+        // eat one char like pacman, then get next_char
+        private char consume_char(char c) { T.assert(fn_body[i] == c, $"expected [{c}], got [{fn_body[i]}]"); return next_char(); }
+        // parse a variable name which may contain numbers
+        private string parse_var_name()
+        {
+            // align the 2 pointers
+            int j = i;
+            // move j forward while there are alphanumeric chars
+            while ((j < fn_body.Length) && is_alphanumeric(fn_body[j])) j++;
+            // between i and j is our alphanumeric identifier
+            string ret = fn_body.Substring(i, j - i);
+            // advance i, should now be at the first non-alphanumeric char
+            i = j;
+            return ret;
+        }
+        // recursive descent parser 
+        private expr parse_expr()
+        {
+            for (char c = curr_char(); c != '$'; c = next_char())
+            {
+                switch (c)
+                {
+                    case ('\\'): // FUNC
+                        consume_char('\\');
+                        string var_name = parse_var_name();
+                        consume_char('.');
+                        expr body = parse_expr();
+                        return new expr(var_name, body);
+                    case ('('): // APP 
+                        consume_char('(');
+                        expr f = parse_expr();
+                        consume_char(' ');
+                        expr e = parse_expr();
+                        consume_char(')');
+                        return new expr(f, e);
+                    default: return new expr(parse_var_name()); // parsing variables here
+                }
+            }
+            throw new Exception("bug in parse_expr");
+        }
+        public expr parse(string def, ref Dictionary<string, expr> fn_table)
+        {
+            i = 0;
+            string[] parts = def.Split('=', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            this.fn_name = parts[0];
+            this.fn_body = parts[1];
+
+            expr ret = parse_expr();
+            fn_table[fn_name] = ret;
+            return ret;
+        }
+        public void load_fn_table(string file)
+        {
+            foreach (string l in System.IO.File.ReadAllLines(file, Encoding.ASCII))
+            {
+                if ((l.StartsWith(';')) || (l.Trim() == "")) continue; // take out comments and empty lines
+                expr rez = this.parse(l, ref expr.fn_table);
+                //Console.WriteLine($"{l}\n{rez}\n\n");
+            }
+        }
+    }
+
+    public static class Tests
+    {
+        // higher level constructors for humans
+        static public expr name(name n) => new expr(n); // name
+        static public expr func(name n, expr e) => new expr(n, e); // function
+        static public expr app(expr f, expr e) => new expr(f, e); // application
+        public static void Go()
+        {
+            var identity = func("x", name("x"));
+            var make_pair = func("first", func("second", func("func", app(app(name("func"), name("first")), name("second")))));
+            var select_first = func("fst", func("snd", name("fst")));
+            var select_second = func("fst", func("snd", name("snd")));
+
+            var tru = select_first;
+            var fls = select_second;
+
+            var cond = func("a", func("b", func("c", app(app(name("c"), name("a")), name("b")))));
+            var not = func("x", app(app(app(cond, fls), tru), name("x")));
+            var and = func("x", func("y", app(app(name("x"), name("y")), fls)));
+            var or = func("x", func("y", app(app(name("x"), tru), name("y"))));
+
+            var zero = identity;
+            var succ = func("n", func("s", app(app(name("s"), fls), name("n")))); // \n.\s.((s false) n)
+
+            var iszero = func("n", app(name("n"), select_first)); // \n.(n \a.\b.a)
+            var pred = func("n", app(app(app(iszero, name("n")), zero), app(name("n"), select_second)));
+
+            var one = app(succ, zero);
+            var two = app(succ, one);
+            var three = app(succ, two);
+            var four = app(succ, three);
+            var five = app(succ, four);
+            var six = app(succ, five);
+            var seven = app(succ, six);
+            var eight = app(succ, seven);
+            var nine = app(succ, eight);
+            var ten = app(succ, nine);
+
+
+            // Y f = \s.(f (s s)) \s.(f (s s))
+            // Y = \f.(\s.(f (s s)) \s.(f (s s)))
+            var s = func("s", app(name("f"), app(name("s"), name("s"))));
+            var Y = func("f", app(s, s));
+
+            // def add1 f x y = if (iszero y) then (x) else f (succ x) (pred y)
+            var if_clause = app(iszero, name("y"));
+            var then_clause = name("x");
+            var _succ_x = app(succ, name("x"));
+            var _pred_y = app(pred, name("y"));
+            var else_clause = app(app(name("f"), _succ_x), _pred_y);
+            var fn_body = app(app(if_clause, then_clause), else_clause);
+            var add1 = func("f", func("x", func("y", fn_body)));
+            var add = app(Y, add1);
+
+
+
+
+
+
+            Parser p = new Parser();
+            string _file = @"C:\Users\pwnag\source\repos\lambda_calc\lambda_calc\standard.lambda";
+            p.load_fn_table(_file);
+
+            var soy = app(app(add, ten), ten);
+
+
+            soy.eval();
+            expr.fn_table["test"].eval();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tests.Go();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+
+(Y id)
+
+(\f.(\s.(f (s s)) \s.(f (s s))) \x.x) 
+
+(\f0.(\s.(f0 (s s)) \s.(f0 (s s))) \x.x) a-conv
+
+(\s.(\x.x (s s)) \s.(\x.x (s s)))  b-reduce
+
+(\s1.(\x.x (s1 s1)) \s.(\x.x (s s)))  a-conv
+
+(\x.x (\s.(\x.x (s s)) \s.(\x.x (s s))))   b-reduce
+
+(\x2.x2 (\s.(\x.x (s s)) \s.(\x.x (s s))))   a-conv
+
+(\s.(\x.x (s s)) \s.(\x.x (s s)))   b-reduce
+
+(\s3.(\x.x (s3 s3)) \s.(\x.x (s s)))   a-conv
+
+(\x.x (\s.(\x.x (s s)) \s.(\x.x (s s))))    b-reduce
+
+repeat 
+
+
+
+
+*/
---
+++ expr.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using name = System.String;
+
+
+namespace lambda_calc
+{
+    public class expr
+    {
+        static public Dictionary<string, expr> fn_table = new Dictionary<name, expr>(); // function table
+        // house keeping
+        private string print(int depth = 0, bool newline = true)
+        {
+            StringBuilder ret = new StringBuilder();
+            if (newline) ret.Append('\n' + new String(' ', depth));
+            switch (this.t)
+            {
+                case (type.NAME): ret.Append(this.n); break;
+                case (type.FUNC):
+                    {
+                        var intro = $"\\{this.n}.";
+                        depth += intro.Length;
+                        ret.Append(intro);
+                        ret.Append(this.e.print(depth, false));
+                        break;
+                    }
+                case (type.APP):
+                    {
+                        ret.Append("( ");
+                        depth += 2;
+                        ret.Append(this.f.print(depth, false));
+                        ret.Append(this.e.print(depth));
+                        ret.Append(" )");
+                        break;
+                    }
+            }
+            return ret.ToString();
+        }
+        public override string ToString() => this.print(0);
+        private void reset_is_evaluated() // recursively sets is_evaluated to false (so we can re-evaluate it w different method)
+        {
+            this.is_evaluated = false;
+            switch (this.t)
+            {
+                case (type.NAME): return; // names are always already evaluated
+                case (type.FUNC): this.e.reset_is_evaluated(); return;
+                case (type.APP): this.f.reset_is_evaluated(); this.e.reset_is_evaluated(); return;
+            }
+        }
+
+
+
+        // optimization
+        public bool is_evaluated = false;
+        public expr evaluated_version;
+
+
+        private enum type { NAME, FUNC, APP };
+        //     n -> n just a variable/name/string/symbol
+        //     \n.e -> n is a bound variable, body is a lambda-expression
+        //     (f e) -> f is the function, e is the argument lambda-expression
+        readonly private type t;
+        readonly private name n; // used as name for type.NAME, and bound variable for type.FUNC
+        readonly private expr f; // only used as (f e) in type.APP
+        readonly private expr e; // used as \n.e in type.FUNC, or (f e) in type.APP
+
+
+        // constructors 
+        public expr(name n) { this.t = type.NAME; this.n = n; } // name
+        public expr(name n, expr e) { this.t = type.FUNC; this.n = n; this.e = e; } // function
+        public expr(expr f, expr e) { this.t = type.APP; this.f = f; this.e = e; } // application
+
+        public expr eval(bool normal = true, bool eval_fn_body = false) // high-level wrapper for evaluate 
+        {
+            T.ac = T.br = T.os = 0; // reset counters
+            expr ret = this.evaluate(normal, eval_fn_body); // eval
+            Console.WriteLine($"\n[ F I N A L   O U T P U T ] :: used {T.br} beta-reductions, {T.ac} alpha-conversions, {T.os} optimization-skips");
+            Console.WriteLine(ret);
+
+            // have to reset table bc durig eval, some expr's pointed to its functions (and 'evaluated' under normal is diff from 'evaluated' under applicative)
+            foreach (name key in fn_table.Keys) fn_table[key].reset_is_evaluated();
+            T.ac = T.br = T.os = 0; // reset counters
+            ret.reset_is_evaluated();
+            expr reta = ret.evaluate(false, true); // applicative order reduce the terminated result, and eval fn bodies
+            Console.WriteLine($"\n[ Applicative Version ] :: used {T.br} beta-reductions, {T.ac} alpha-conversions, {T.os} optimization-skips");
+            Console.WriteLine(reta);
+
+            return ret;
+        }
+
+
+        private expr subs(name nam, expr e_new) // only called by itself, and in eval during beta-reduction and a-conversion
+        {
+            // WHEN BETA-REDUCING, WE ARE ALREADY IN THE BODY OF THE FUNCTION, SO THE "BOUNDED" VAR IS "FREE" HERE. 
+            switch (this.t)
+            {
+                // expr is a name. If names match, return the substitute, else the original name. 
+                case (type.NAME): return this.n == nam ? e_new : this;
+
+                // APP: substitute both f and e 
+                case (type.APP): return new expr(this.f.subs(nam, e_new), this.e.subs(nam, e_new));
+
+                // FUNC: \f.\f.(...) -> return \f.(...), aka nothing got alpha-converted. otherwise subs body
+                case (type.FUNC): return this.n == nam ? this : new expr(this.n, this.e.subs(nam, e_new));
+            }
+            throw new Exception("bug in subs");
+        }
+
+        public expr evaluate(bool normal, bool eval_fn_body)
+        {
+            //if (this.is_evaluated) { T.os++; return this.evaluated_version; } // optimization
+            T.br++;
+            //Console.WriteLine($"\n\n{this.t}\n{this}");
+
+            switch (this.t)
+            {
+                case (type.NAME): // replace names by user defined functions if they are in the fn_table
+                    {
+                        this.evaluated_version = expr.fn_table.ContainsKey(this.n) ? expr.fn_table[this.n].evaluate(normal, eval_fn_body) : this;
+                        break;
+                    }
+                case (type.FUNC): // TAPL says we don't eval inside fn body, even under applicative mode; so i added special option for it
+                    {
+                        this.evaluated_version = eval_fn_body ? new expr(this.n, this.e.evaluate(normal, eval_fn_body)) : this;
+                        break;
+                    }
+                case (type.APP):
+                    {
+                        // evaluate f. 
+                        expr f_ev = this.f.evaluate(normal, eval_fn_body);
+
+                        if (f_ev.t == type.FUNC) // f is FUNC, BETA-REDUCE
+                        {
+                            // alpha-convert in prep for beta-reduction, since we don't use deBruijn
+                            name new_name = T.generate_name(this.n);
+
+                            // in the case of \f.(\f.f \f.f) substitution will stop upon entering nested fn with same bound variable
+                            expr f_ev_ac = f_ev.subs(this.n, new expr(new_name));
+
+                            // evaluate argument if we are not in normal mode 
+                            expr arg = normal ? this.e : this.e.evaluate(normal, eval_fn_body);
+
+                            // we a-converted f_eval, so whatever is in arg won't name clash with it. 
+                            this.evaluated_version = f_ev_ac.e.subs(f_ev_ac.n, arg).evaluate(normal, eval_fn_body);
+                        }
+                        // if f is not a function, return (f <whatever>) and eval the RHS (couldn't reduce the APP)
+                        else this.evaluated_version = new expr(f_ev, this.e.evaluate(normal, eval_fn_body));
+
+                        break;
+                    }
+            }
+            this.is_evaluated = true; // the Lambda-Expression now has an evaluated version attached to it
+            return this.evaluated_version;
+        }
+    }
+}
